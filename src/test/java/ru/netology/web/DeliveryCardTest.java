cd788071e69a834141a73e6d272b5f6de270de24
kris-i-kris <flare120491@gmail.com>
2024-01-29 14:00:13 +0700

    url

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -2,10 +2,14 @@ package ru.netology.web;
 
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
+
 class DeliveryCardTest {
+
     @Test
     void test1() {
-        open(http://localhost:9999/);
+        open("http://localhost:9999/");
 
     }
 
